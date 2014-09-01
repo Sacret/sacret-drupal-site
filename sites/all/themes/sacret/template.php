8fc2d558237868e94a06cd5edf24bc8050e03ba8
Kozaeva Nastia <kozaeva_n@progforce.com>
2014-09-02 00:21:34 +0400

    sacret theme changes

---
+++ sites/all/themes/sacret/template.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * template.php
+ */
+
+function sacret_preprocess_image_style(&$vars) {
+  if (isset($vars['style_name']) && ($vars['style_name'] == 'new_image' || $vars['style_name'] == 'progs_image' || $vars['style_name'] ==  'gallery_image_full')) {
+    $vars['attributes']['class'][] = 'img-responsive'; // can be 'img-rounded', 'img-circle', or 'img-thumbnail'
+  }
+}
